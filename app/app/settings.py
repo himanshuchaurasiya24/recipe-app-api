c958e9581767010262dc02a4d5bc6b60fe16fdff
Himanshu Chaurasiya <himanshuchaurasiya24@gmail.com>
2026-05-03 11:16:59 +0530

    adde some code

--- app/app/settings.py
+++ app/app/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import sys
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -74,15 +75,30 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        "ENGINE":"django.db.backends.postgresql",
-        'HOST':os.environ.get("DB_HOST"),
-        'NAME':os.environ.get("DB_NAME"),
-        'USER':os.environ.get("DB_USER"),
-        'PASSWORD':os.environ.get("DB_PASS"),
+if 'test' in sys.argv:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+elif os.environ.get("DB_HOST") and os.environ.get("DB_NAME"):
+    DATABASES = {
+        'default': {
+            "ENGINE": "django.db.backends.postgresql",
+            'HOST': os.environ.get("DB_HOST"),
+            'NAME': os.environ.get("DB_NAME"),
+            'USER': os.environ.get("DB_USER"),
+            'PASSWORD': os.environ.get("DB_PASS"),
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
--- app/app/test.py
+++ app/app/test.py
@@ -5,4 +5,4 @@ from app import calc
 class CalcTest(TestCase):
     def test_add_numbers(self):
         res = calc.add(2, 3)
-        self.assertEqual(res, 1)
+        self.assertEqual(res, 5)
