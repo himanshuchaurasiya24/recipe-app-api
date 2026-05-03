a8fd4203b6af833c8599ab2b0a87cd2b932b3cbf
Himanshu Chaurasiya <himanshuchaurasiya24@gmail.com>
2026-05-03 21:58:40 +0530

    Fix email validation in UserManager.create_user to reject whitespace-only emails

--- app/app/settings.py
+++ app/app/settings.py
@@ -88,6 +88,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+AUTH_USER_MODEL = 'core.User'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
