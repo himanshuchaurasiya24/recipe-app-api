a5e54b710ccd652aba3d6ce349210ff23bfae0e7
Himanshu Chaurasiya <himanshuchaurasiya24@gmail.com>
2026-05-04 09:09:07 +0530

    working fine delete data if server not starting

--- app/app/settings.py
+++ app/app/settings.py
@@ -88,7 +88,6 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
-AUTH_USER_MODEL = 'core.User'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -129,3 +128,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
