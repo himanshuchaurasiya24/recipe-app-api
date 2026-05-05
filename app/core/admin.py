7d5456b2158ec3ddc3c97f1892fcaf1500b19109
Himanshu Chaurasiya <himanshuchaurasiya24@gmail.com>
2026-05-05 21:24:27 +0530

    added documentation

--- app/core/admin.py
+++ app/core/admin.py
@@ -11,24 +11,26 @@ class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
     ordering = ['id']
     list_display = ['email', 'name']
-    fieldsets=(
+    fieldsets = (
         (None, {
-            'fields':('email','password')
+            'fields': ('email', 'password')
         }),
-        (_('Permissions'),{
-            'fields':('is_active', 'is_staff', 'is_superuser')
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser')
         }),
         (_('Important dates'), {
-            'fields':('last_login',)
+            'fields': ('last_login',)
         }),
     )
-    readonly_fields=['last_login']
-    add_fieldsets=(
+    readonly_fields = ['last_login']
+    add_fieldsets = (
         (None, {
-            'classes':('wide',),
-            'fields':('email', 'password1', 'password2','name', 'is_active', 'is_staff', 'is_superuser',
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'name',
+                       'is_active', 'is_staff', 'is_superuser',
             )
         }),
     )
+
 
 admin.site.register(models.User, UserAdmin)
